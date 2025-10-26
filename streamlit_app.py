83bd1484f20126620477e314bb55514b0ee5bffa
VladaSilaeva <VladaSilaeva@yandex.ru>
2025-10-26 15:12:16 +0300

    Тестовая база данных

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,37 @@
 import streamlit as st
+import sqlite3
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("My plant-catalog app")
+
+
+@st.cache_resource
+def get_connection():
+    return sqlite3.connect("plants.db", check_same_thread=False)
+
+connection = get_connection()
+cursor = connection.cursor()
+
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Plants (
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+rus_name TEXT
 )
+''')
+
+connection.commit()
+
+
+
+if st.button("Add row"):
+    cursor.execute('INSERT INTO Plants (name) VALUES (?)', ('Oxalis',))
+    connection.commit()
+
+if st.button("Update row"):
+    cursor.execute('UPDATE Plants SET rus_name = ? WHERE name = ?', ('Кислица', 'Oxalis',))
+    connection.commit()
+
+cursor.execute("SELECT * FROM Plants")
+rows = cursor.fetchall()
+st.write(rows)
